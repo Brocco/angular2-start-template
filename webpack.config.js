4a6d029a81a1af958eba188a8aa2431670bb84ca
Gabriel <gabo.obregon@gmail.com>
2015-12-03 21:59:19 -0500

    Using webpack dev server for development instead of live server.
    Builds now only happen once when `npm start` is run. Webpack dev server
    will also reload on changes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,45 @@
+var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-	entry: {
-    'vendor': [
-      // Angular 2 Deps
-      '@reactivex/rxjs',
-      'zone.js',
-      'reflect-metadata',
-      // to ensure these modules are grouped together in one file
-      'angular2/angular2',
-      'angular2/core',
-      'angular2/router',
-      'angular2/http'
-    ],
-		'app': [
-			'./src/bootstrap'
-		]
-	},
-	output: {
-		path: './dist/js',
-		filename: '[name].js',
-		sourceMapFilename: '[name].js.map',
-    chunkFilename: '[id].chunk.js'
-	},
-	resolve: {
-		extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
-	},
-	module: {
-		loaders: [
-			{ test: /\.ts$/, loader: 'ts-loader' }
-		]
-	},
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'vendor', /* filename= */'vendor.js')
-  ]
+    entry: {
+        'vendor': [
+            // Angular 2 Deps
+            '@reactivex/rxjs',
+            'zone.js',
+            'reflect-metadata',
+            // to ensure these modules are grouped together in one file
+            'angular2/angular2',
+            'angular2/core',
+            'angular2/router',
+            'angular2/http'
+        ],
+        'app': [
+            './src/bootstrap'
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve('dist'),
+        publicPath: '/js',
+        port: 9000,
+        inline: true,
+        noInfo: true
+    },
+    output: {
+        path: path.resolve('dist/js'),
+        filename: '[name].js',
+        sourceMapFilename: '[name].js.map',
+        chunkFilename: '[id].chunk.js'
+    },
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, include: /src/, loader: 'ts-loader' }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'vendor', /* filename= */'vendor.js')
+    ]
 };
