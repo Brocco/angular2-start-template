111a3525e3b40383a6fe81eaa09c16a5226f87fa
Mike Brocchi <mbrocchi@gmail.com>
2015-12-03 23:31:24 -0500

    Updated angular2 to version 2.0.0-alpha.47

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,4 +1,5 @@
-import {bootstrap, provide} from 'angular2/angular2';
+import {bootstrap} from 'angular2/platform/browser';
+import {provide} from 'angular2/core';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {App} from './components/app/app';
